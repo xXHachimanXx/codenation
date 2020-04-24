eccf015576759c775d355d7c3b0572a420e499ec
xXHachimanXx <globaldep90@gmail.com>
2020-04-24 11:35:53 -0300

    Teste de commit.

--- java-7/src/main/java/challenge/Cor.java
+++
@@ -1,5 +0,0 @@
-package challenge;
-
-public enum Cor {
-    PRETO,BRANCO,COLORIDO
-}
--- java-7/src/main/java/challenge/EstacionamentoException.java
+++
@@ -1,8 +0,0 @@
-package challenge;
-
-public class EstacionamentoException extends RuntimeException {
-
-    public EstacionamentoException(String message) {
-        super(message);
-    }
-}
