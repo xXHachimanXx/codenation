363e282393b9285ee00e5134fe7cd50a7b1deca2
xXHachimanXx <globaldep90@gmail.com>
2020-04-28 14:47:51 -0300

    Implementando função para encontrar promoção válida para o conjunto de produtos.

--- nodejs-12/src/index.js
+++ nodejs-12/src/index.js
@@ -1,4 +1,3 @@
-// const productsList = require("./data/products.json");
 const promotions = ['SINGLE LOOK', 'DOUBLE LOOK', 'TRIPLE LOOK', 'FULL LOOK'];
 
 /**
@@ -21,15 +20,63 @@ function getProductsFromDataBase(ids, productList) {
 	return products;
 }
 
+/**
+ * Função que retorna o tipo de promoção que será aplicada
+ * dada a lista de produtos com suas categorias.
+ * 
+ * @param {Array} products obrigatório
+ * @returns {String} promoção
+ */
 function getPromotion(products) {
+	let looks = [0, 0, 0, 0]; // Array contador
+	let biggest = 0; // Maior dos contadores
+	let promotion = 0; // Endereço da promocao
+
+	products.forEach(element => {
+		// Escolher contador de look
+		switch (element.category) {
+			case promotions[0]: // Single Look
+				++looks[0]; 
+				break;
+			case promotions[1]: // Double Look
+				++looks[1]; 
+				break;
+			case promotions[2]: // Triple Look
+				++looks[2]; 
+				break;
+			default: // Full Look
+				++looks[3]; 
+				break;
+		}		
+	});
+
+	biggest = looks.reduce(0, (a, b) => { a > b ? a : b}); // pegar maior valor
+	promotion = looks.indexOf((element) => element === biggest ); // Pegar endereço do promoção
 	
+	return promotions[promotion];
+}
+
+
+function getPriceValues(products, promotion) {
+	var totalPrice = 0;
+	var discountValue = 0;
+	var discountPercentage = 0.0;
+	var regularPrice = 0;
+
+	products.forEach(product => {
+		regularPrice += product.regularPrice;
+		discountValue += getPriceWithPromotion(product, promotion);
+	});
 }
 
 function getShoppingCart(ids, productsList) {
 	var products = getProductsFromDataBase(ids, productsList);
 	var promotion = getPromotion(products);	
+	var priceValues = getPriceValues(products, promotion);
+	
 
-	return {products, };
+
+	return {products, promotion, };
 }
 
 module.exports = {
