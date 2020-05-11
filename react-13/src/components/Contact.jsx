f1e53b3a32950e59927e2b3cb1a30ad3e95d89f8
xXHachimanXx <globaldep90@gmail.com>
2020-05-11 14:08:25 -0300

    Formatando datas de admissão.

--- react-13/src/components/Contact.jsx
+++ react-13/src/components/Contact.jsx
@@ -10,9 +10,19 @@ class Contact extends React.Component {
     this.avatar = props.avatar;
     this.company = props.company;
     this.departament = props.departament;
-    this.admissionDate = props.admissionDate;
+    this.admissionDate = this.formatDate(props.admissionDate);
     this.phone = props.phone;
     this.country = props.country;
+  }
+
+  // Formatar data
+  formatDate(date) {
+    var formatedDate = new Date(date);
+    var year = formatedDate.getFullYear();
+    var month = (formatedDate.getMonth() + 1).toString().padStart(2, '0');
+    var day = formatedDate.getDate().toString().padStart(2, '0');
+
+    return `${day}/${month}/${year}`;
   }
 
   render() {
