da98c9cfe0cd6b5a6e169f2d593b4847d867cee3
xXHachimanXx <globaldep90@gmail.com>
2020-05-26 20:26:23 -0300

    Organizando arquivos para o próximo desafio.

---
+++ fashionista/src/containers/Topbar/Topbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import "./Topbar.css";
+
+function Topbar() {
+    return(
+        <div className="Topbar">
+            <header class="header">
+                <img src="" alt="" srcset=""/>
+            </header>
+        </div>
+    );
+}
+
+export default Topbar;
