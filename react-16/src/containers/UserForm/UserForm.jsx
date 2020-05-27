4138ee0d40b05e4e86dacd89948a8edebec54f11
xXHachimanXx <globaldep90@gmail.com>
2020-05-27 14:28:58 -0300

    Renderizando Topbar.

---
+++ react-16/src/containers/UserForm/UserForm.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+import SuccessMessage from '../../components/SuccessMessage';
+
+import './UserForm.scss';
+
+const UserForm = () => {
+  return (
+    <React.Fragment>
+      <form data-testid="user-form"></form>
+    </React.Fragment>
+  );
+};
+
+export default UserForm;
