6b45a29c9ad0efe6434a9f39ca8f7f665baf19d0
xXHachimanXx <globaldep90@gmail.com>
2020-05-29 15:14:54 -0300

    Configurando rota de profile.

--- react-16/src/containers/UserProfile/UserProfile.jsx
+++ react-16/src/containers/UserProfile/UserProfile.jsx
@@ -5,6 +5,19 @@ import './UserProfile.scss';
 const UserProfile = ({ avatar, name, username }) => {
   return (
     <section className="profile" data-testid="user-profile">
+      <div className="profile-data">
+        <div className="user">
+          <figure className="user__thumb">
+            <img src={avatar} alt={name} />
+          </figure>
+
+          <p className="user__name">
+            {name}
+            {username && <span>@{username}</span>}
+          </p>
+
+        </div>
+      </div>
     </section>
   )
 };
--- react-16/src/routes/ProfileRoute/ProfileRoute.jsx
+++ react-16/src/routes/ProfileRoute/ProfileRoute.jsx
@@ -5,9 +5,56 @@ import UserPosts from '../../containers/UserPosts';
 
 import Loading from '../../components/Loading';
 
+import { BASE_LINK } from "../../utils/constants.js";
+import { useParams } from 'react-router-dom';
+
 const ProfileRoute = () => {
+  const [user, setUser] = useState({});
+  const [userId, setUserId] = useState('');
+  const [userAvatar, setUserAvatar] = useState('');
+  const [userNameState, setUserNameState] = useState('');
+  const [userNick, setUserNick] = useState('');
+
+  const [posts, setPosts] = useState([]);
+  const {userName} = useParams();  
+
+  const { pathname } = window.location;
+  const param = pathname.split("/")[2];
+
+  useEffect( () => {
+    fetch(BASE_LINK + `users?search=${param}`)
+      .then(res => res.json())
+      .then(data => {
+        setUser(data[0]);
+        setUserAvatar(data[0].avatar);
+        setUserNameState(data[0].name);
+        setUserNick(data[0].username);
+        setUserId(data[0].id);
+      })
+
+    console.log(userName);
+  }, [userName]);
+
+  useEffect( () => {
+    fetch(BASE_LINK + `users/${userId}/posts`)
+      .then(res => res.json())
+      .then(data => setPosts(data))    
+  }, [user]);
+
   return (
-    <div data-testid="profile-route">      
+    <div data-testid="profile-route">
+      {        
+        user ? (          
+          <>
+          <UserProfile
+            avatar={userAvatar}
+            name={userNameState}
+            username={userNick}
+          />          
+          <UserPosts posts={posts} />
+          </>
+        ) : (<Loading />)
+      }
     </div>
   );
 };
