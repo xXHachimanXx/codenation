71aec4115a2317accb9af8932dc54978234480b0
xXHachimanXx <globaldep90@gmail.com>
2020-06-01 21:18:08 -0300

    Criando arquivos de rotas.

--- fashionista/src/App.js
+++ fashionista/src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import './App.css';
 
-import Topbar from "./components/Topbar/Topbar";
-
 function App() {
   return (
     <div className="App">
-      <Topbar />
     </div>
   );
 }
