334b8f7b2da2121777a9a5c6a53bfcb72cf2a3b2
xXHachimanXx <globaldep90@gmail.com>
2020-06-05 16:45:26 -0300

    Estilizando scrollbar.

--- fashionista/src/containers/Drawer/Drawer.jsx
+++ fashionista/src/containers/Drawer/Drawer.jsx
@@ -5,23 +5,25 @@ import "./Drawer.css";
 const SizeButton = (counter) => {
   return (
     <div className="drawer">
-      <header className="drawer-header">
+      <header className="drawer__header">
         <div className="app__container">
-          <div className="drawer-header__title">
-            <div className="drawer-header__icons">
-              <button className="drawer-header__button--back">
+          <div className="drawer__header__title">
+            <div className="drawer__header__icons">
+              <button className="drawer__header__button--back">
                 <i class="fas fa-arrow-left"></i>
               </button>
             </div>
 
-            <div className="drawer-header__title-info">Sacola 0</div>
+            <div className="drawer__header__title__info">Sacola (0)</div>
           </div>
         </div>
       </header>
 
-      <div className="drawer-content"></div>
+      <div className="drawer__content">
+        <div className="drawer__product-list"></div>
+      </div>
 
-      <div className="drawer-footer"></div>
+      <div className="drawer__footer"></div>
     </div>
   );
 };
